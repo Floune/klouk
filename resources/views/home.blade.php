2b5a88570027d8fa6963011de33010ff83a2dfa7
RobinFloune <rfournier@simplonprod.co>
2020-04-26 11:32:45 +0200

    dqfffh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
     @include('partials.hall-of-shame')
     <div>
         <marquee>fesse</marquee>
-    </div>-->
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
